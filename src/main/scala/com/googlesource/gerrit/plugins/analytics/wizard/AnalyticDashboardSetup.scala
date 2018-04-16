4d8c411cdd8421782cc3e124528f27dc7d1c010c
Fabio Ponciroli <ponch78@gmail.com>
2018-04-16 17:02:32 +0200

    Automatically setup an analytics dashboard
    This plugin allows to add a step in the github gerrit workflow configuration
    to automatically setup an analytics dashboard.
    
    At the moment it is creating a customised docker-compose file that can
    be used to spin up the resuorces needed for the dashboard.
    
    Change-Id: If77502e0ff3b28355b87eed2e7fb0b65dbaf1eda

---
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/wizard/AnalyticDashboardSetup.scala
@@ -0,0 +1,28 @@
+package com.googlesource.gerrit.plugins.analytics.wizard
+
+case class AnalyticDashboardSetup(name: String, config: String)
+
+object AnalyticDashboardSetup {
+
+  val dockerComposeTemplate = { (name: String) =>
+    s"""
+       |version: '3'
+       |services:
+       |  kibana:
+       |    build: kibana
+       |    container_name: "kibana-for-${name}-project"
+       |    environment:
+       |      SERVER_BASEPATH: "/kibana"
+       |    depends_on:
+       |      - elasticsearch
+       |  elasticsearch:
+       |    build: elasticsearch
+       |    container_name: "es-for-${name}-project"
+       |    environment:
+       |      - ES_JAVA_OPTS=-Xmx4g -Xms4g
+       |      - http.host=0.0.0.0
+       |    volumes:
+       |      - es-indexes:/usr/share/elasticsearch/data
+     """.stripMargin
+  }
+}
