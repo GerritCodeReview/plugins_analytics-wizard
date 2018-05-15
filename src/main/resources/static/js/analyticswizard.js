725451c925d4e6cb0da6e5e98950b8349b25555c
Fabio Ponciroli <ponch78@gmail.com>
2018-05-15 16:19:37 +0100

    Check dashboard status
    Check the status of the dashboard, i.e.: checks if
    the services run by the docker compose are up
    
    Change-Id: I6a708345915d6e0959966e5122520d6590572d86

--- src/main/resources/static/js/analyticswizard.js
+++ src/main/resources/static/js/analyticswizard.js
@@ -31,6 +31,36 @@ function submitDetailsForm() {
     });
 }
 
+var handle201Status = function(command) {
+  $("#up-error-alert").hide();
+  $("#down-error-alert").hide();
+  $("#up-ok-alert").hide();
+  $("#down-ok-alert").hide();
+  $("#" + command + "-ok-alert").show();
+};
+
+function dashboardService(command) {
+    var projectName = $("#input-project-name").val();
+    $.ajax({
+      type : "POST",
+      url : `/a/projects/${projectName}/analytics-wizard~server`,
+      dataType: 'application/json',
+      // Initially project-dashboard is a 1 to 1 relationship
+      data: "{'action': '" + command + "'}",
+      contentType:"application/json; charset=utf-8",
+      // Need to catch the status code since Gerrit doesn't return
+      // a well formed JSON, hence Ajax treats it as an error
+      statusCode: {
+        201: handle201Status(command)
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        if(jqXHR.status != 201) {
+          handleError()
+        }
+      }
+    });
+}
+
 function showConfigDetails() {
     var projectName = $("#input-project-name").val();
     $.ajax({
