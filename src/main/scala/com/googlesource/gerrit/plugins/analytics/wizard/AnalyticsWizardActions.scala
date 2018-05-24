2f9cd1b99e016b9ffbf269d4ea40694361ed4335
Fabio Ponciroli <ponch78@gmail.com>
2018-05-24 08:25:44 +0100

    URL Encode project name
    Project name need to be URL encoded to allow Gerrit REST API
    to find the resource.
    
    Furthermore Docker compose doesn't like container names with '/',
    hence we replace it with '-' and add a timestamp to make them unique.
    
    Bug: Issue 9067
    Change-Id: I4fe990ff20946a43e08a2f1505676d36b1514ffd

--- src/main/scala/com/googlesource/gerrit/plugins/analytics/wizard/AnalyticsWizardActions.scala
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/wizard/AnalyticsWizardActions.scala
@@ -13,10 +13,12 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.analytics.wizard
 
+import java.net.URLEncoder
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.Path
 
 import com.google.common.io.ByteStreams
+import com.google.gerrit.extensions.annotations.PluginData
 import com.google.gerrit.extensions.restapi.{
   Response,
   RestApiException,
@@ -25,12 +27,12 @@ import com.google.gerrit.extensions.restapi.{
 }
 import com.google.gerrit.server.project.ProjectResource
 import com.google.inject.Inject
-import AnalyticDashboardSetup.writer
-import com.google.gerrit.extensions.annotations.PluginData
+import com.googlesource.gerrit.plugins.analytics.wizard.AnalyticDashboardSetup.writer
 
-import scala.io.Source
+import scala.util.Try
 
-class GetAnalyticsStack @Inject()(@PluginData val dataPath: Path) extends RestReadView[ProjectResource] {
+class GetAnalyticsStack @Inject()(@PluginData val dataPath: Path)
+    extends RestReadView[ProjectResource] {
   override def apply(
       resource: ProjectResource): Response[AnalyticDashboardSetup] = {
 
@@ -50,8 +52,16 @@ class PutAnalyticsStack @Inject()(@PluginData val dataPath: Path)
                      input: Input): Response[String] = {
 
     val projectName = resource.getControl.getProject.getName
-    AnalyticDashboardSetup(projectName, dataPath.resolve(s"docker-compose.${projectName}.yaml")).createDashboardSetupFile()
-    Response.created(s"Dashboard configuration created for $projectName!")
+    AnalyticsWizardActions
+      .TryURLEncoder(projectName)
+      .fold(throw new RestApiException("Unsupported Encoding Exception")) {
+        name =>
+          AnalyticDashboardSetup(
+            projectName,
+            dataPath.resolve(s"docker-compose.${name}.yaml"))
+            .createDashboardSetupFile()
+          Response.created(s"Dashboard configuration created for $name!")
+      }
   }
 }
 
@@ -62,24 +72,36 @@ class PostAnalyticsStack @Inject()(@PluginData val dataPath: Path)
                      input: DockerComposeCommand): Response[String] = {
 
     val projectName = resource.getControl.getProject.getName
-    val pb = new ProcessBuilder(
-      "docker-compose",
-      "-f",
-      s"${dataPath.toFile.getAbsolutePath}/docker-compose.${projectName}.yaml",
-      input.action.toLowerCase)
-    pb.redirectErrorStream(true)
+    AnalyticsWizardActions
+      .TryURLEncoder(projectName)
+      .fold(throw new RestApiException("Unsupported Encoding Exception")) {
+        name =>
+          val pb = new ProcessBuilder(
+            "docker-compose",
+            "-f",
+            s"${dataPath.toFile.getAbsolutePath}/docker-compose.${name}.yaml",
+            input.action.toLowerCase)
+          pb.redirectErrorStream(true)
 
-    val ps: Process = pb.start
-    ps.getOutputStream.close
-    val output = new String(ByteStreams.toByteArray(ps.getInputStream), UTF_8)
-    ps.waitFor
+          val ps: Process = pb.start
+          ps.getOutputStream.close
+          val output =
+            new String(ByteStreams.toByteArray(ps.getInputStream), UTF_8)
+          ps.waitFor
 
-    ps.exitValue match {
-      case 0 => Response.created(output)
-      case _ =>
-        throw new RestApiException(
-          s"Failed with exit code: ${ps.exitValue} - $output")
-    }
+          ps.exitValue match {
+            case 0 => Response.created(output)
+            case _ =>
+              throw new RestApiException(
+                s"Failed with exit code: ${ps.exitValue} - $output")
+          }
+      }
 
   }
+}
+
+object AnalyticsWizardActions {
+  // URLEncoder could potentially throw UnsupportedEncodingException
+  def TryURLEncoder(name: String): Option[String] =
+    Try { URLEncoder.encode(name, "UTF-8") }.toOption
 }
