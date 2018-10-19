51b8da1333d25a16c6f4ded73f2388c4af05ced7
Fabio Ponciroli <ponch78@gmail.com>
2018-10-19 15:42:54 +0200

    Receive ETL configuration parameter
    Modify 'PUT analytics-wizard~stack' to receive the ETL configuration
    paramenters.
    
    Change-Id: I57790c40937de8d2f485e887c18185f9873c3634

---
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/wizard/model/ETLConfigRaw.scala
@@ -0,0 +1,69 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.analytics.wizard.model
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import scala.util.Try
+
+class ETLConfigRaw private (aggregate: String,
+                            projectPrefix: String,
+                            since: String,
+                            until: String,
+                            url: String,
+                            eventsPath: String,
+                            emailAliasesPath: String,
+                            writeNotProcessedEventsTo: String,
+                            username: String,
+                            password: String)
+
+object ETLConfigRaw {
+  def apply(aggregate: String,
+            projectPrefix: String,
+            since: String,
+            until: String,
+            url: String,
+            eventsPath: String,
+            emailAliasesPath: String,
+            writeNotProcessedEventsTo: String,
+            username: String,
+            password: String): Either[ETLConfigValidationError, ETLConfig] = {
+    for {
+      s <- validateLocalDate(since).right
+      u <- validateLocalDate(until).right
+    } yield
+      ETLConfig(projectPrefix = projectPrefix,
+                since = s,
+                until = u,
+                username = username,
+                password = password)
+  }
+
+  private def validateLocalDate(input: String): Either[ETLConfigValidationError, LocalDate] = {
+    val formatter      = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val maybeLocalDate = Try(LocalDate.parse(input, formatter)).toOption
+    Either.cond(maybeLocalDate.isDefined,
+                maybeLocalDate.get,
+                LocalDateValidationError("since", input))
+  }
+}
+
+trait ETLConfigValidationError {
+  def message: String = s"Error validating '$parameter' parameter: $value"
+  def value: String
+  def parameter: String
+}
+case class LocalDateValidationError(parameter: String, value: String)
+    extends ETLConfigValidationError
---
+++ src/test/scala/com/googlesource/gerrit/plugins/analytics/wizard/model/ETLConfigRawSpec.scala
@@ -0,0 +1,73 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.analytics.wizard.model
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ETLConfigRawSpec extends FlatSpec with Matchers {
+
+  behavior of "ETLConfigRaw"
+
+  it should "pass validation with correct parameters" in {
+    ETLConfigRaw(
+      aggregate = "sd",
+      projectPrefix = "prefix",
+      since = "2018-10-10",
+      until = "2018-10-13",
+      url = "http",
+      eventsPath = "events",
+      emailAliasesPath = "path",
+      writeNotProcessedEventsTo = "path",
+      username = "dss",
+      password = "dsd"
+    ).isRight shouldBe true
+  }
+
+  it should "fail validation for invalid 'since' parameter" in {
+    val invalidDate = "randomString"
+    val eitherETLConfig = ETLConfigRaw(
+      aggregate = "sd",
+      projectPrefix = "prefix",
+      since = invalidDate,
+      until = "2018-10-13",
+      url = "http",
+      eventsPath = "events",
+      emailAliasesPath = "path",
+      writeNotProcessedEventsTo = "path",
+      username = "dss",
+      password = "dsd"
+    )
+    eitherETLConfig.isLeft shouldBe true
+    eitherETLConfig.left.get shouldBe LocalDateValidationError("since", invalidDate)
+  }
+
+  it should "fail validation for invalid 'until' parameter" in {
+    val invalidDate = "randomString"
+    val eitherETLConfig = ETLConfigRaw(
+      aggregate = "sd",
+      projectPrefix = "prefix",
+      since = "2018-10-13",
+      until = invalidDate,
+      url = "http",
+      eventsPath = "events",
+      emailAliasesPath = "path",
+      writeNotProcessedEventsTo = "path",
+      username = "dss",
+      password = "dsd"
+    )
+    eitherETLConfig.isLeft shouldBe true
+    eitherETLConfig.left.get shouldBe LocalDateValidationError("since", invalidDate)
+  }
+
+}
