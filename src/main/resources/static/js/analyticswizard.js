075a36324c732c4b811c7f24128f57e1ab462987
Antonio Barone <syntonyze@gmail.com>
2018-10-29 16:06:28 +0200

    Dummy change
    Soon to be abandoned.
    
    Change-Id: I3be3ab4284775e9ad7a92e540f3b990d7bad4710

--- src/main/resources/static/js/analyticswizard.js
+++ src/main/resources/static/js/analyticswizard.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 function showSuccessWithText(text) {
     $("#error-alert").hide();
     $("#success-alert").html(text).show();
